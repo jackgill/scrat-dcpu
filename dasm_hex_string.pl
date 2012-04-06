1a26b80231ee2a5919e5611f79f9686f537cb300
Jack Gill <jack@jackmgill.com>
2012-04-06 14:39:46 -0600

    Utility for disassembling a single word instruction represented as a ehx string

---
+++ dasm_hex_string.pl
@@ -0,0 +1,24 @@
+use strict;
+use warnings;
+
+use DCPU;
+
+# Parse command line arguments
+if (@ARGV != 1 || !($ARGV[0] =~ /(0x)?[0-9a-fA-F]{4}/)) {
+	die "Usage: $0 (hex string representing a single 16 bit word";
+}
+
+my $hex_string = $ARGV[0];
+my $bin_string = hex2bitstring($hex_string);
+my $assembly = disassemble_instruction($bin_string);
+
+print "$assembly\n";
+
+sub hex2bitstring {
+	my $hex = shift;
+	$hex =~ s/^0x//;
+	$hex =~ s/^\s+//;
+	$hex =~ s/\s+$//;
+	my $num = hex($hex);
+	return sprintf("%016b", $num);
+}	
